8cd51fd9cc4b2e76b540ac93e4a72bb3cf7a0b42
Baraa Hegazy <hegazyba@msu.edu>
2020-02-19 20:55:20 -0500

    added features

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Baraa Hegazy on 2/19/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
