b68447120e152bcf6c7e0e8395fe7485b0d8f6cd
Baraa Hegazy <hegazyba@msu.edu>
2020-02-26 14:01:05 -0500

    fixed retweet api call

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -96,7 +96,7 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
     
     func retweet(tweetId:Int, success: @escaping () -> (), failure: @escaping (Error) -> ())
     {
-        let url = "https://api.twitter.com/1.1/retweet/\(tweetId)id.json"
+        let url = "https://api.twitter.com/1.1/statuses/retweet/\(tweetId).json"
         TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
             success()
         }, failure: { (task: URLSessionDataTask?, error: Error) in
